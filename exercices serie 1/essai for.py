6b4e49b219bae5aadb29d9486370284cbec6ab3e
Frédéric LURET <47600423+fluret@users.noreply.github.com>
2024-10-04 14:34:21 +0000

    04102024 16h34

---
+++ exercices serie 1/essai for.py
@@ -0,0 +1,11 @@
+for c in "Frederic":
+    print(c)
+
+for c in (1, 2, 3, 5):
+    print(c)
+    
+for c in [(1, 2), (23, 14), (2, 3)]:
+    print(c)
+
+for c in [(1, 2), (23, 14), (2, 3)]:
+    print(c[0])
