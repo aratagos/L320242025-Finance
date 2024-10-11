96978af4ea4e7cc9cf6a0234b100630b32a22ffc
Frédéric LURET <47600423+fluret@users.noreply.github.com>
2024-10-11 15:35:58 +0000

    Dernier TD

--- exercices serie 1/ex-02-06-bis.py
+++ exercices serie 1/ex-02-06-bis.py
@@ -1,10 +1,13 @@
 n = int(input('Entrez la taille du damier : '))
 
+damier = []
 for i in range(n):
+    ligne = []
     for j in range(n):
         if (i+j) % 2 == 0:
-            print('X', end='')
+            ligne.append('X')
         else:
-            print('O', end='')
-    print()
+            ligne.append('O')
+    damier.append(ligne)
+
 
